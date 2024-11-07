f84176d6867b7a6b4bd0148002a8b1f6c8b1aa3c
marizqlav <marizqlav@alum.us.es>
2024-11-07 18:35:56 +0100

    fix: Added terms and privacy policy acceptance checks to card creation and editing tests

--- cypress/e2e/cardDetailEdit.cy.js
+++ cypress/e2e/cardDetailEdit.cy.js
@@ -29,6 +29,9 @@ describe("testing the edit card functionality", () => {
       typeAndAssert("input[name='password']", "@Password1");
       typeAndAssert("input[name='password2']", "@Password1");
       typeAndAssert("input[name='email']", email);
+      cy.get("input[id='terms']").check();
+      cy.get("input[id='priv']").check();
+
       cy.get("button").contains("Registrar").click().wait(1500);
 
       cy.wait(2000);
@@ -103,6 +106,9 @@ describe("testing the edit card functionality with errors", () => {
       typeAndAssert("input[name='password']", "@Password1");
       typeAndAssert("input[name='password2']", "@Password1");
       typeAndAssert("input[name='email']", email);
+      cy.get("input[id='terms']").check();
+      cy.get("input[id='priv']").check();
+
       cy.get("button").contains("Registrar").click().wait(1500);
 
       cy.wait(2000);
@@ -162,6 +168,9 @@ describe("testing the edit card functionality with errors", () => {
       typeAndAssert("input[name='password']", "@Password1");
       typeAndAssert("input[name='password2']", "@Password1");
       typeAndAssert("input[name='email']", email);
+      cy.get("input[id='terms']").check();
+      cy.get("input[id='priv']").check();
+
       cy.get("button").contains("Registrar").click().wait(1500);
 
       cy.wait(2000);
--- cypress/e2e/createCard.cy.js
+++ cypress/e2e/createCard.cy.js
@@ -28,6 +28,8 @@ describe("testing the create card text", () => {
       typeAndAssert("input[name='password']", "@Password1");
       typeAndAssert("input[name='password2']", "@Password1");
       typeAndAssert("input[name='email']", email);
+      cy.get("input[id='terms']").check();
+      cy.get("input[id='priv']").check();
       cy.get("button").contains("Registrar").click().wait(1500);
 
       cy.wait(2000);
@@ -78,6 +80,9 @@ describe("testing the create card text", () => {
       typeAndAssert("input[name='password']", "@Password1");
       typeAndAssert("input[name='password2']", "@Password1");
       typeAndAssert("input[name='email']", email);
+      cy.get("input[id='terms']").check();
+      cy.get("input[id='priv']").check();
+
       cy.get("button").contains("Registrar").click().wait(1500);
 
       cy.wait(2000);
@@ -135,6 +140,9 @@ describe("testing the create card functionality error", () => {
       typeAndAssert("input[name='password']", "@Password1");
       typeAndAssert("input[name='password2']", "@Password1");
       typeAndAssert("input[name='email']", email);
+      cy.get("input[id='terms']").check();
+      cy.get("input[id='priv']").check();
+
       cy.get("button").contains("Registrar").click().wait(1500);
 
       cy.wait(2000);
