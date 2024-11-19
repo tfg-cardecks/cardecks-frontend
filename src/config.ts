5553f25f08b8148d8d20052f35be9f9c1dc39ce3
marizqlav <marizqlav@alum.us.es>
2024-11-19 18:43:53 +0100

    fix: Updated API_URL to point to the correct staging server

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-//export const API_URL = "http://localhost:5000";
+export const API_URL = "http://localhost:5000";
 //export const API_URL = "https://ejmeplo-brfjhbc2fkf4hfgb.northeurope-01.azurewebsites.net";
-export const API_URL = "https://cardecks-backlog-production.up.railway.app";
+//export const API_URL = "https://cardecks-backlog-production.up.railway.app";
