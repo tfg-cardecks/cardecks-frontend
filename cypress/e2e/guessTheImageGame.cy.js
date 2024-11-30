6cbb815b45c9c90ab3b5f49a817527f8e20bae29
marizqlav <marizqlav@alum.us.es>
2024-11-30 20:59:24 +0100

    test: Improved error handling in GuessTheImageGame API responses and added new card data for 'arbol'

--- cypress/e2e/guessTheImageGame.cy.js
+++ cypress/e2e/guessTheImageGame.cy.js
@@ -20,21 +20,44 @@ function startGame() {
     .wait(500);
 }
 
-function playGame() {
-  clickToNavElement("Mazos");
-  cy.get("a").contains("Crear Mazo").click().wait(2000);
+function clickToCorrectAnswer(jsonImages, jsonContents) {
+  cy.get('.image-container img').invoke('attr', 'src').then((src) => {
+    cy.get('.options-container button').each(($button, index, $list) => {
+      cy.wrap($button).invoke('text').then((text) => {
+        const buttonText = text.trim().split('. ')[1].toUpperCase();
+        if (jsonImages.includes(src)) {
+          const imageIndex = jsonImages.indexOf(src);
+          const correctButtonText = jsonContents[imageIndex];
+          if (buttonText === correctButtonText) {
+            cy.wrap($button).click().wait(500);
+            return false;
+          }
+        }
+      });
+    });
+  });
+  cy.get("button").contains("Enviar Respuesta").click().wait(500);
+  cy.get(".swal2-confirm").click();
+  cy.get("button").contains("Siguiente").click().wait(1000);
+}
 
-  typeAndAssert("input[name='name']", generateRandomText());
-  typeAndAssert("input[name='description']", generateRandomText());
-  typeAndAssert("input[name='theme']", generateRandomText());
-  cy.get("button").contains("Crear Mazo").click().wait(2000);
+function restartCounter() {
+  cy.get("h2")
+    .contains("Estadísticas")
+    .next()
+    .next()
+    .children()
+    .next()
+    .get("button")
+    .contains("Resetear Contador")
+    .click()
+    .wait(1000)
+    .then(() => {
+      cy.get(".swal2-confirm").click();
+    });
+}
 
-  selectedFileToImportAndSubmit("cypress/e2e/json/nubes.json");
-  selectedFileToImportAndSubmit("cypress/e2e/json/cascada.json");
-  selectedFileToImportAndSubmit("cypress/e2e/json/hojas.json");
-  selectedFileToImportAndSubmit("cypress/e2e/json/lluvia.json");
-  selectedFileToImportAndSubmit("cypress/e2e/json/nubosa.json");
-
+function goToCatalogoDeJuegos() {
   cy.visit("http://localhost:5173/lobby").wait(2000);
   cy.get(".game-type-list")
     .find("img[alt='Adivina la Imagen']")
@@ -45,8 +68,8 @@ function playGame() {
 
   startGame();
   cy.get(".swal2-confirm").click();
-
   cy.wait(2000);
+  cy.get("button").contains("Volver al Catálogo de Juegos").click().wait(2000);
 }
 
 beforeEach(() => {
@@ -54,221 +77,126 @@ beforeEach(() => {
 });
 
 describe("testing guessTheImageGame", () => {
-  // it("can`t play guessTheImageGame (not enought cards)", () => {
-  //   clickToNavElement("Mazos");
-  //   cy.get("a").contains("Crear Mazo").click().wait(2000);
+  it("can`t play guessTheImageGame (not enought cards)", () => {
+    clickToNavElement("Mazos");
+    cy.get("a").contains("Crear Mazo").click().wait(2000);
 
-  //   typeAndAssert("input[name='name']", generateRandomText());
-  //   typeAndAssert("input[name='description']", generateRandomText());
-  //   typeAndAssert("input[name='theme']", generateRandomText());
-  //   cy.get("button").contains("Crear Mazo").click().wait(2000);
+    typeAndAssert("input[name='name']", generateRandomText());
+    typeAndAssert("input[name='description']", generateRandomText());
+    typeAndAssert("input[name='theme']", generateRandomText());
+    cy.get("button").contains("Crear Mazo").click().wait(2000);
 
-  //   cy.get("h2")
-  //     .contains("Cartas")
-  //     .next()
-  //     .find('input[type="file"]')
-  //     .selectFile("cypress/e2e/json/cascada.json");
-  //   cy.get("button").contains("Importar Carta a Mazo").click().wait(2000);
+    cy.get("h2")
+      .contains("Cartas")
+      .next()
+      .find('input[type="file"]')
+      .selectFile("cypress/e2e/json/cascada.json");
+    cy.get("button").contains("Importar Carta a Mazo").click().wait(2000);
 
-  //   cy.visit("http://localhost:5173/lobby").wait(2000);
-  //   cy.get(".game-type-list")
-  //     .find("img[alt='Adivina la Imagen']")
-  //     .click()
-  //     .wait(2000);
+    cy.visit("http://localhost:5173/lobby").wait(2000);
+    cy.get(".game-type-list")
+      .find("img[alt='Adivina la Imagen']")
+      .click()
+      .wait(2000);
 
-  //   cy.get(".container").children().next().find("h2").eq(0).click().wait(500);
+    cy.get(".container").children().next().find("h2").eq(0).click().wait(500);
 
-  //   startGame();
-  // });
+    startGame();
+  });
 
-  describe("testing guessTheImageGame", () => {
-    it("can play guessTheImageGame and show error alert", () => {
-      // Interceptar la solicitud para crear el juego y obtener el ID del juego
-      clickToNavElement("Mazos");
-      cy.get("a").contains("Crear Mazo").click().wait(2000);
+});
 
-      typeAndAssert("input[name='name']", generateRandomText());
-      typeAndAssert("input[name='description']", generateRandomText());
-      typeAndAssert("input[name='theme']", generateRandomText());
-      cy.get("button").contains("Crear Mazo").click().wait(2000);
+describe("testing guessTheImageGame", () => {
+  it("can play guessTheImageGame and complete the game", () => {
+    clickToNavElement("Mazos");
+    cy.get("a").contains("Crear Mazo").click().wait(2000);
 
-      selectedFileToImportAndSubmit("cypress/e2e/json/nubes.json");
-      selectedFileToImportAndSubmit("cypress/e2e/json/cascada.json");
-      selectedFileToImportAndSubmit("cypress/e2e/json/hojas.json");
-      selectedFileToImportAndSubmit("cypress/e2e/json/lluvia.json");
-      selectedFileToImportAndSubmit("cypress/e2e/json/nubosa.json");
+    typeAndAssert("input[name='name']", generateRandomText());
+    typeAndAssert("input[name='description']", generateRandomText());
+    typeAndAssert("input[name='theme']", generateRandomText());
+    cy.get("button").contains("Crear Mazo").click().wait(2000);
 
-      cy.visit("http://localhost:5173/lobby").wait(2000);
-      cy.get(".game-type-list")
-        .find("img[alt='Adivina la Imagen']")
-        .click()
-        .wait(2000);
-      cy.intercept("POST", "/api/guessTheImageGames").as("createGame");
+    selectedFileToImportAndSubmit("cypress/e2e/json/nubes.json");
+    selectedFileToImportAndSubmit("cypress/e2e/json/cascada.json");
+    selectedFileToImportAndSubmit("cypress/e2e/json/hojas.json");
+    selectedFileToImportAndSubmit("cypress/e2e/json/lluvia.json");
+    selectedFileToImportAndSubmit("cypress/e2e/json/arbol.json");
 
-      cy.get(".container").children().next().find("h2").eq(0).click().wait(500);
+    cy.visit("http://localhost:5173/lobby").wait(2000);
+    cy.get(".game-type-list")
+      .find("img[alt='Adivina la Imagen']")
+      .click()
+      .wait(2000);
 
-      startGame();
-      cy.get(".swal2-confirm").click();
+    cy.get(".container").children().next().find("h2").eq(0).click().wait(500);
 
-      cy.wait(2000);
-      cy.wait("@createGame", { timeout: 10000 }).then((interception) => {
-        // Registrar la respuesta completa para depuración
-        cy.log("Interception:", JSON.stringify(interception.response.body));
-        const guessTheImageGameId =
-          interception.response.body.guessTheImageGameId;
-        cy.wrap(guessTheImageGameId).as("guessTheImageGameId");
-      });
+    startGame();
+    cy.get(".swal2-confirm").click();
 
-      // Interceptar la solicitud para obtener los datos del juego usando el ID del juego
-      cy.get("@guessTheImageGameId").then((guessTheImageGameId) => {
-        cy.log(
-          "Interception responseNew:",
-          JSON.stringify(guessTheImageGameId)
-        );
-        cy.intercept("GET", `/api/guessTheImageGame/${guessTheImageGameId}`).as(
-          "getGameData"
-        );
-     
-        cy.log("Interception responseNew123:", guessTheImageGameId);
-        // Esperar a que se realice la solicitud y capturar la respuesta correcta
-        cy.wait("@getGameData").then((interception) => {
-          cy.log(
-            "Interception responseNew1234:",
-            JSON.stringify(interception.response.body)
-          );
+    cy.wait(2000);
 
-          const correctAnswer = interception.response.body.correctAnswer; // Ajusta esto a la estructura real de la respuesta
-          cy.wrap(correctAnswer).as("correctAnswer");
-        });
+    const files = [
+      'cypress/e2e/json/nubes.json',
+      'cypress/e2e/json/cascada.json',
+      'cypress/e2e/json/hojas.json',
+      'cypress/e2e/json/lluvia.json',
+      'cypress/e2e/json/arbol.json'
+    ];
 
-        cy.get(".options-container").should("be.visible");
+    let jsonContents = [];
+    let jsonImages = [];
 
-
-
-
-
-
-
-        
-        // Obtener la respuesta correcta y seleccionar una opción incorrecta
-        cy.get("@correctAnswer").then((correctAnswer) => {
-          cy.get(".option-button").each(($button) => {
-            const optionText = $button.text().trim();
-            if (optionText !== correctAnswer) {
-              cy.wrap($button).click();
-              return false; // Rompe el bucle each después de seleccionar una opción incorrecta
-            }
-          });
-        });
-
-        console.log("Respuesta incorrecta seleccionada");
-        // Enviar la respuesta
-        cy.get("button").contains("Enviar Respuesta").click().wait(500);
-
-        // Verificar que se muestra el mensaje de error
-        cy.get(".swal2-popup").should("be.visible").contains("¡Incorrecto!");
+    files.forEach((file) => {
+      cy.readFile(file).then((json) => {
+        const content = json.frontSide.text[0].content.toUpperCase();
+        const imageUrl = json.backSide.images[0].url;
+        jsonContents.push(content);
+        jsonImages.push(imageUrl);
       });
     });
 
-    // cy.get(".options-container").should("be.visible");
+    for (let i = 0; i <= 25; i++) {
+      cy.log("Intento: " + i);
+      clickToCorrectAnswer(jsonImages, jsonContents);
+    }
+    cy.get(".swal2-confirm").click();
 
-    // // Obtener la respuesta correcta
-    // cy.get(".options-container").then(($container) => {
-    //   const correctAnswer = $container.data("correct-answer"); // Suponiendo que la respuesta correcta está almacenada en un atributo de datos
-    //   console.log("Respuesta correcta:", correctAnswer);
-
-    //   // Seleccionar una opción incorrecta
-    //   cy.get(".option-button").each(($button) => {
-    //     const optionText = $button.text().trim();
-    //     if (optionText !== correctAnswer) {
-    //       cy.wrap($button).click();
-    //       return false; // Rompe el bucle each después de seleccionar una opción incorrecta
-    //     }
-    //   });
-    // });
-    // console.log("Respuesta incorrecta seleccionada");
-    // // Enviar la respuesta
-    // cy.get("button").contains("Enviar Respuesta").click().wait(500);
-
-    // // Verificar que se muestra el mensaje de error
-    // cy.get(".swal2-popup").should("be.visible").contains("¡Incorrecto!");
-
-    // cy.get(".swal2-confirm").click();
-
-    // cy.get("button").contains("Siguiente").click().wait(1000);
-    // cy.get("button")
-    //   .contains("Volver al Catálogo de Juegos")
-    //   .click()
-    //   .wait(1000);
-
-    // cy.get(".game-type-list")
-    //   .find("img[alt='Juego del Ahorcado']")
-    //   .click()
-    //   .wait(2000);
-
-    // cy.get(".container").children().next().find("h2").eq(0).click().wait(500);
-
-    // startGame();
-
-    // cy.get(".container")
-    //   .children()
-    //   .next()
-    //   .next()
-    //   .next()
-    //   .find("button")
-    //   .contains("Continuar Juego en Progreso")
-    //   .click()
-    //   .wait(500);
-
-    // cy.get("button").contains("Forzar Completado").click().wait(1000);
+    restartCounter();
+    goToCatalogoDeJuegos();
   });
 
-  // it("can play hangmanGame (show Juego completado alert)", () => {
-  //   playGame();
-  //   cy.get('.options-container').should('be.visible');
 
-  //   // Obtener la respuesta correcta
-  //   cy.get('.options-container').then(($container) => {
-  //     const correctAnswer = $container.data('correct-answer'); // Suponiendo que la respuesta correcta está almacenada en un atributo de datos
-  //     console.log('Respuesta correcta:', correctAnswer);
+  it("can play guessTheImageGame and show ¡Incorrecto! error and then ff game", () => {
+    clickToNavElement("Mazos");
+    cy.get("a").contains("Crear Mazo").click().wait(2000);
 
-  //     // Seleccionar la opción correcta
-  //     cy.get('.option-button').each(($button) => {
-  //       const optionText = $button.text().trim();
-  //       if (optionText === correctAnswer) {
-  //         cy.wrap($button).click().wait(2000);;
-  //         return false; // Rompe el bucle each después de seleccionar la opción correcta
-  //       }
-  //     });
-  //   });
-  //   console.log("Respuesta correcta seleccionada");
-  //   // Enviar la respuesta
-  //   cy.get("button").contains("Enviar Respuesta").click().wait(500);
+    typeAndAssert("input[name='name']", generateRandomText());
+    typeAndAssert("input[name='description']", generateRandomText());
+    typeAndAssert("input[name='theme']", generateRandomText());
+    cy.get("button").contains("Crear Mazo").click().wait(2000);
 
-  //   // Verificar que se muestra el mensaje de éxito
-  //   cy.get(".swal2-popup").should('be.visible').contains('¡Correcto!');
-  //   // let i;
-  //   // for (i = 0; i <= 25; i++) {
-  //   //   cy.log("Intento: " + i);
-  //   //   guessWord();
-  //   // }
+    selectedFileToImportAndSubmit("cypress/e2e/json/nubes.json");
+    selectedFileToImportAndSubmit("cypress/e2e/json/cascada.json");
+    selectedFileToImportAndSubmit("cypress/e2e/json/hojas.json");
+    selectedFileToImportAndSubmit("cypress/e2e/json/lluvia.json");
+    selectedFileToImportAndSubmit("cypress/e2e/json/arbol.json");
 
-  //   // if (i === 26) {
-  //   //   cy.get(".swal2-confirm").click();
-  //   // }
-  // });
+    cy.visit("http://localhost:5173/lobby").wait(2000);
+    cy.get(".game-type-list")
+      .find("img[alt='Adivina la Imagen']")
+      .click()
+      .wait(2000);
 
-  // it("can go to information hangmanGame", () => {
-  //   cy.visit("http://localhost:5173/lobby").wait(2000);
-  //   cy.get(".game-type-list")
-  //     .find("img[alt='Juego del Ahorcado']")
-  //     .click()
-  //     .wait(2000);
+    cy.get(".container").children().next().find("h2").eq(0).click().wait(500);
 
-  //   cy.get(".container").children().next().find("h2").eq(0).click().wait(500);
+    startGame();
+    cy.get(".swal2-confirm").click();
 
-  //   cy.contains("button", "Información").click().wait(2000);
+    cy.wait(2000);
 
-  //   cy.contains("button", "Ocultar Información").click().wait(2000);
-  // });
+    cy.get('.options-container button').eq(0).click().wait(500);
+    cy.get("button").contains("Enviar Respuesta").click().wait(500);
+    cy.get(".swal2-confirm").click();
+    cy.get("button").contains("Forzar Completado").click().wait(500);
+  });
 });
